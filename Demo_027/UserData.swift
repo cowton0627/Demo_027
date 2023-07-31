0afa359f3c8cfe8c7f0f63b4774ad2635cd63802
Charles Cheng <charles_cheng@microjet.com.tw>
2023-07-31 23:13:35 +0800

    modify delegation pattern and closure passing data

---
+++ Demo_027/UserData.swift
@@ -0,0 +1,14 @@
+//
+//  UserData.swift
+//  Demo_027
+//
+//  Created by Chun-Li Cheng on 2023/7/31.
+//
+
+import UIKit
+
+struct UserData {
+    var avatar: UIImage
+    var gender: String
+    var customColor: UIColor
+}
--- Demo_027/ViewController.swift
+++ Demo_027/ViewController.swift
@@ -7,40 +7,62 @@
 
 import UIKit
 
+extension UIStoryboard {
+    
+    convenience init(name: String) {
+        self.init(name: name, bundle: nil)
+    }
+    
+}
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var myAvatarImgView: UIImageView!
-    @IBOutlet weak var chooseBtn: UIButton!
-    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var choosingBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        chooseBtn.layer.cornerRadius = chooseBtn.bounds.height / 2
+//        chooseBtn.layer.cornerRadius = chooseBtn.bounds.height / 2
         
     }
 
     @IBAction func chooseBtnTapped(_ sender: Any) {
-        let controller = storyboard?.instantiateViewController(withIdentifier:  "\(MyTableViewController.self)") as! MyTableViewController
-//        controller.delegate = self
-        controller.myClosure = { [weak self] passData in
-            self?.myAvatarImgView.image = passData.avatar
-            self?.myLabel.text = passData.gender
-            self?.view.backgroundColor = passData.myColor
-        }
-        present(controller, animated: true, completion: nil)
+        // this storyboard is default, so it's Main Storyboard
+        // just like the code snippet: let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // we can use extension UIStoryboard, let storyboard = UIStoryboard(name: "Main")
+//        let storyboard = UIStoryboard(name: "Main")
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "\(MyTableViewController.self)") as? MyTableViewController else { return }
+        
+        // MARK: - passing data using delegation pattern
+        vc.delegate = self
+        
+        // MARK: - passing data using closure
+//        vc.choosingCompletion = { [weak self] userData in
+//            self?.avatarImageView.image = userData.avatar
+//            self?.genderLabel.text = userData.gender
+//            self?.view.backgroundColor = userData.customColor
+//        }
+        
+        present(vc, animated: true, completion: nil)
+//        show(vc, sender: nil)
     }
     
     
+    
     @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
-        
+        // this is unwind segue, usually using passing data in segue scenario
+        // unwind segue scenario use to passing data from reverse transition
     }
 
 }
 
-//extension ViewController: AvatarSelectionDelegate {
-//    func didTapChoice(image: UIImage, gender: String, color: UIColor) {
-//        myAvatarImgView.image = image
-//        myLabel.text = gender
-//        view.backgroundColor = color
-//    }
-//}
+extension ViewController: AvatarChoosingDelegate {
+    
+    func didChoosing(image: UIImage, gender: String, color: UIColor) {
+        avatarImageView.image = image
+        genderLabel.text = gender
+        view.backgroundColor = color
+    }
+}
